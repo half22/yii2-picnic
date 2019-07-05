d9adc82931607568bbe025d38006bb2ef771226e
Pavol Dragun <half@half.sk>
2019-07-05 13:20:58 +0200

    fix

--- src/extensions/jquery.js
+++ src/extensions/jquery.js
@@ -6,10 +6,12 @@
         distance = distance || 10;
         times = times || 4;
 
+        this.css({position: 'relative'});
         for(var i = 0; i < (times + 1) ; i++)
         {
-            this.animate({ left: ((i % 2 == 0 ? distance : distance * -1))}, interval);
+            this.animate({left: ((i % 2 == 0 ? distance : distance * -1))}, interval);
         }
+        this.animate({left: 0}, interval);
     }
 
 }(window, jQuery));
