4df84ffda28a2f911d737155f9715c908f06a80b
half22 <github@half.sk>
2020-01-08 13:03:03 +0100

    fix

--- src/extensions/jquery.js
+++ src/extensions/jquery.js
@@ -20,15 +20,26 @@
         {
             this.css({transform: ''});
 
-            var matrix = this.css('transform').match(/^matrix\((.+)\)$/)[1] || this.css('transform').match(/^matrix3d\((.+)\)$/)[1];
+            var matrix = this.css('transform').match(/^matrix\((.+)\)$/)[1];
             if(matrix)
             {
                 var values = matrix.split(',');
-                for(var i = 0; i <= values.length; i++)
+                for(var i = 0; i < values.length; i++)
                 {
                     values[i] = Math.round(values[i]);
                 }
                 this.css('transform', 'matrix(' + values.join(', ') + ')');
+            }
+
+            var matrix3d = this.css('transform').match(/^matrix3d\((.+)\)$/)[1];
+            if(matrix3d)
+            {
+                var values3d = matrix3d.split(',');
+                for(var j = 0; j < values3d.length; j++)
+                {
+                    values3d[j] = Math.round(values3d[j]);
+                }
+                this.css('transform', 'matrix3d(' + values3d.join(', ') + ')');
             }
         }
     };
