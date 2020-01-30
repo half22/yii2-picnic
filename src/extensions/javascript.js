5ba4a39c12c6eb2b37c8eabd9fd4570c315982cd
half22 <github@half.sk>
2020-01-30 15:29:31 +0100

    fix

--- src/extensions/javascript.js
+++ src/extensions/javascript.js
@@ -1,26 +1,27 @@
-(function(window) {
+(function (window)
+{
 
-    window.ucfirst = function(string)
+    window.ucfirst = function (string)
     {
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
 
-    window.isUndefined = function(value)
+    window.isUndefined = function (value)
     {
         return typeof value === 'undefined' || value === null;
     };
 
-    window.isDefined = function(value)
+    window.isDefined = function (value)
     {
         return !isUndefined(value);
     };
 
-    window.isFunction = function(value)
+    window.isFunction = function (value)
     {
         return typeof value === 'function';
     };
 
-    window.camelCase = function(string)
+    window.camelCase = function (string)
     {
         return string.replace(/^([A-Z])|[\s-_](\w)/g, function (match, p1, p2, offset)
         {
@@ -29,26 +30,26 @@
         });
     };
 
-    window.getTransitionEndEvent = function(element)
+    window.getTransitionEndEvent = function (element)
     {
         var transitions = {
-            'transition' : 'transitionend',
-            'WebkitTransition' : 'webkitTransitionEnd',
-            'MozTransition' : 'transitionend',
-            'OTransition' : 'oTransitionEnd otransitionend'
+            'transition': 'transitionend',
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd otransitionend'
         };
         var domElement = element.get(0);
 
-        for(var t in transitions)
+        for (var t in transitions)
         {
-            if(domElement.style[t] !== undefined)
+            if (domElement.style[t] !== undefined)
             {
                 return transitions[t];
             }
         }
     };
 
-    window.isMobile = function()
+    window.isMobile = function ()
     {
         return ('ontouchstart' in document.documentElement);
     };
@@ -58,7 +59,7 @@
         return !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
     };
 
-    window.isWebpSupported = function()
+    window.isWebpSupported = function ()
     {
         var elem = document.createElement('canvas');
 
@@ -68,6 +69,58 @@
         }
 
         return false;
+    };
+
+    //IE polyfill
+    if (!Array.prototype.find)
+    {
+        Object.defineProperty(Array.prototype, 'find', {
+            value: function (predicate)
+            {
+                // 1. Let O be ? ToObject(this value).
+                if (this == null)
+                {
+                    throw new TypeError('"this" is null or not defined');
+                }
+
+                var o = Object(this);
+
+                // 2. Let len be ? ToLength(? Get(O, "length")).
+                var len = o.length >>> 0;
+
+                // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+                if (typeof predicate !== 'function')
+                {
+                    throw new TypeError('predicate must be a function');
+                }
+
+                // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+                var thisArg = arguments[1];
+
+                // 5. Let k be 0.
+                var k = 0;
+
+                // 6. Repeat, while k < len
+                while (k < len)
+                {
+                    // a. Let Pk be ! ToString(k).
+                    // b. Let kValue be ? Get(O, Pk).
+                    // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+                    // d. If testResult is true, return kValue.
+                    var kValue = o[k];
+                    if (predicate.call(thisArg, kValue, k, o))
+                    {
+                        return kValue;
+                    }
+                    // e. Increase k by 1.
+                    k++;
+                }
+
+                // 7. Return undefined.
+                return undefined;
+            }
+        });
     }
+
 
 }(window));
