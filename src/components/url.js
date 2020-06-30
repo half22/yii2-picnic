f8a6b4d6a4281ec8462953d1e6db46b9ad7af713
half22 <github@half.sk>
2020-06-30 13:39:38 +0200

    fix

--- src/components/url.js
+++ src/components/url.js
@@ -56,9 +56,14 @@
             while (e = re.exec(query))
             {
                 var k = decode(e[1]), v = decode(e[2]);
-                if (k.substring(k.length - 2) === '[]' || k.match(/.+\[\d\]/))
+                if (k.substring(k.length - 2) === '[]')
                 {
                     k = k.substring(0, k.length - 2);
+                    (params[k] || (params[k] = [])).push(v);
+                }
+                if (k.match(/.+\[\d+\]/))
+                {
+                    k = k.replace(/\[\d+\]/, '');
                     (params[k] || (params[k] = [])).push(v);
                 }
                 else params[k] = v;
