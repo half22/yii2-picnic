4484987e62ee37acaa8d359ed9f6374cce04d7ae
half22 <github@half.sk>
2020-06-30 13:30:14 +0200

    fix

--- src/components/url.js
+++ src/components/url.js
@@ -56,7 +56,7 @@
             while (e = re.exec(query))
             {
                 var k = decode(e[1]), v = decode(e[2]);
-                if (k.substring(k.length - 2) === '[]')
+                if (k.substring(k.length - 2) === '[]' || k.match(/.+\[\d\]/))
                 {
                     k = k.substring(0, k.length - 2);
                     (params[k] || (params[k] = [])).push(v);
