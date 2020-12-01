285ee92aea25fc15e9c44d3df9c74fb60623c6f1
Pavol Dragun <half@half.sk>
2020-12-01 14:49:49 +0100

    fix

--- src/plugins/floating-label.js
+++ src/plugins/floating-label.js
@@ -36,8 +36,7 @@
                         if(input.data('select2-id'))
                         {
                             label.on('click', function() {
-                                input.select2('open');
-                                return false;
+                                setTimeout(function () { input.select2('open'); }, 0);
                             });
                             input.on('select2:open', function() { onSelect2Open(element); });
                             input.on('select2:close', function() { onSelect2Close(element); });
