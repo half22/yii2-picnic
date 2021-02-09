7944a70e3e313c1f715d51f90388ec2fd7bbd9ad
Pavol Dragun <half@half.sk>
2021-02-09 09:27:14 +0100

    fix

--- src/plugins/floating-label.js
+++ src/plugins/floating-label.js
@@ -86,6 +86,18 @@
                                 input.on('select2:select', function () { onSelect2Toggle(element, input); });
                                 input.on('select2:clear', function () { onSelect2Toggle(element, input); });
                                 onSelect2Toggle(element, input);
+
+                                // on first focus (bubbles up to document), open the menu
+                                $(document).on('focus', '.select2-selection.select2-selection--single', function (e) {
+                                    $(this).closest(".select2-container").siblings('select:enabled').select2('open');
+                                });
+
+                                // steal focus during close - only capture once and stop propogation
+                                $('select.select2').on('select2:closing', function (e) {
+                                    $(e.target).data("select2").$selection.one('focus focusin', function (e) {
+                                        e.stopPropagation();
+                                    });
+                                });
                             }
                         }
 
