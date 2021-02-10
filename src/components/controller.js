bc7b70926eae343751343a065d5c2956ab76f677
Pavol Dragun <half@half.sk>
2021-02-10 06:46:55 +0100

    fix

--- src/components/controller.js
+++ src/components/controller.js
@@ -127,7 +127,7 @@
 
         replaceRoot: function(html)
         {
-            var root = $(html);
+            var root = $('<div>' + html + '</div>', '*[data-controller=' + this.root.data('controller') + ']');
             this.root.replaceWith(root);
             this.root = root;
             this.refresh();
